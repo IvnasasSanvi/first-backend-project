b18c6622512cb6cd8e50225e73565a6e7e13629f
SANVI <sanvijsr1@gmail.com>
2026-02-02 18:21:10 +0530

    backend project

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+// import cors from 'cors';
+// import bodyParser from 'body-parser';
+// import userRoutes from './routes/userRoutes.js';
+// import productRoutes from './routes/productRoutes.js';  
+const app = express();
+
+app.use(express.json());
+
+app.post('/todo', (req, res) => {
+
+})
+
+app.get('/todos', (req, res) => {
+
+})
+
+app.put('/completed', (req, res) => {
+
+})
