b7797c1ff2345c901dbec7fd1b6d3ff8741d916f
SANVI <sanvijsr1@gmail.com>
2026-02-03 17:20:04 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ import express from 'express';
 // import bodyParser from 'body-parser';
 // import userRoutes from './routes/userRoutes.js';
 // import productRoutes from './routes/productRoutes.js';  
+const {createTodo} = require('./types.js');
 const app = express();
 
 app.use(express.json());
 
 app.post('/todo', (req, res) => {
-
+    const createPayload = req.body;
+    const parsePayload  = createTodo.safeParse(createPayload);
 })
 
 app.get('/todos', (req, res) => {
