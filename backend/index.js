342ce1e9086ccc2d6162ebf2af613a22b8255a64
SANVI <sanvijsr1@gmail.com>
2026-02-03 19:33:03 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 // import bodyParser from 'body-parser';
 // import userRoutes from './routes/userRoutes.js';
 // import productRoutes from './routes/productRoutes.js';  
-const {createTodo} = require('./types.js');
+const {createTodo, updateTodo} = require('./types.js');
 const app = express();
 
 app.use(express.json());
@@ -11,6 +11,12 @@ app.use(express.json());
 app.post('/todo', (req, res) => {
     const createPayload = req.body;
     const parsePayload  = createTodo.safeParse(createPayload);
+    if(!parsePayload.success){
+        return res.status(411).json({
+            msg: "You sent the wrong inputs",
+        });
+        return;
+    }
 })
 
 app.get('/todos', (req, res) => {
@@ -18,5 +24,12 @@ app.get('/todos', (req, res) => {
 })
 
 app.put('/completed', (req, res) => {
-
+    const updatePayload = req.body;
+    const parsePayload = updateTodo.safeParse(updatePayload);
+    if(!parsePayload.success){
+        return res.status(411).json({
+            msg: "You sent the wrong inputs",
+        });
+        return;
+    }
 })
